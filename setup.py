fbc6046b5494ffd1d06448fb294babc97ce1decb
opengeostat <adrian.martinez@opengeostat.com>
2016-05-13 12:27:12 -0700

    fixing setup

--- setup.py
+++ setup.py
@@ -4,23 +4,10 @@ Charttable: HTML Tables with bars charts for exploratory data analysis
 Copyright 2016, Adrian Martinez Vargas.
 Licensed under GPL.
 """
-import warnings
-import sys
-from setuptools.command.test import test as TestCommand
-from numpy.distutils.core import Extension
 
 
-# This is a plug-in for setuptools that will invoke py.test
-# when you run python setup.py test
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
+from distutils.core import setup
 
-    def run_tests(self):
-        import pytest  
-        sys.exit(pytest.main(self.test_args))
 
 # define properties for setup
 """ 
@@ -40,42 +27,23 @@ classifiers=[
             'Development Status :: 1 - Alpha',
             'Programming Language :: Python',
             'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'Topic :: Scientific/Engineering :: Mathematics',
+            'License :: OSI Approved :: GPL License',
+            'Topic :: Scientific/Engineering :: Mathematics']
 keywords='html tables EDA'
 author='Adrian Martinez Vargas'
 author_email='adriangeologo@yahoo.es'
 url='https://github.com/opengeostat/charttable'
  
-        
-if __name__ == '__main__':
-     
-    #Cython code extension
-    #-------------------------------------------------------------------
-    from distutils.core import setup    # this is the standard setup
-    from distutils.extension import Extension as CYExtension
-    import numpy
-	
-    charttable = CYExtension( 'charttable.charttable', 
-                            ['cython_code/charttable.pyx'], 
-							include_dirs=[numpy.get_include()]) 
-                               
                             
-    setup(name=name,
-          version=version,
-          description= description,
-          long_description=long_description,
-          classifiers=classifiers,
-          keywords=keywords, 
-          author=author,
-          author_email=author_email,
-          url=url,
-          license='GPL',
-          packages=find_packages(exclude=['examples', 'tests']),
-          include_package_data=True,
-          zip_safe=False,
-          tests_require=['pandas>=0.17', 'nose', 'mock'],
-          cmdclass={'test': PyTest},   
-          install_requires=['pandas'],
-          ext_modules = [charttable])
+setup(name=name,
+      version=version,
+      description= description,
+      long_description=long_description,
+      classifiers=classifiers,
+      keywords=keywords, 
+      author=author,
+      author_email=author_email,
+      url=url,
+      license='GPL',
+      packages=['charttable'],)
 
